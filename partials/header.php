c19422a17b3708a399002bc82deef09ed22298f9
Analia Mok <axm2580@g.rit.edu>
2018-02-10 15:26:44 -0500

    Now making ajax request for repo data

--- partials/header.php
+++ partials/header.php
@@ -4,10 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Analia Mok | GitHub Repositories</title>
   <link href="https://fonts.googleapis.com/css?family=Lato%7CRaleway:400,500,900" rel="stylesheet">
   <link rel="stylesheet" href="css/ionicons.css">
   <link rel="stylesheet" href="css/ionicons.min.css">
   <link rel="stylesheet" href="css/style.css">
-  <title>Analia Mok | GitHub Repositories</title>
+  <script
+  src="http://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+  <script src="js/main.js"></script>
 </head>
 <body>
